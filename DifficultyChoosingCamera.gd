417336849b4869a3af285cc6eb0458f55d8d0191
Kaushik Duddala <kaushik.duddala.964@k12.friscoisd.org>
2023-03-09 02:56:25 -0600

    Working on new level with basic info

--- DifficultyChoosingCamera.gd
+++ DifficultyChoosingCamera.gd
@@ -7,7 +7,7 @@ onready var player = get_parent().get_parent().get_node("YSort/Player")
 func _physics_process(_delta):
 	if(overlaps_body(player)): 
 		set_physics_process(false)
-		body.set_limit(0, -270)
+		body.set_limit(0, -360)
 		body.set_limit(1, -360)
 		body.set_limit(2, 450)
 		body.set_limit(3, -180)
---
+++ TopLeftSign.gd
@@ -0,0 +1,16 @@
+extends Area2D
+
+onready var body = get_parent().get_node("Player")
+onready var sprite = get_parent().get_node("Player/Camera2D/CanvasLayer/CommonName")
+
+
+func _physics_process(_delta):
+	if(overlaps_body(body)): 
+		sprite.set_visible(true)
+	else:
+		sprite.set_visible(false)
+
+
+func _ready():
+	set_physics_process(true)
+
