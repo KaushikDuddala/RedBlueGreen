e0a5713a503693a0b600baa4d61e867fb8c574a5
Kaushik Duddala <kaushik.duddala.964@k12.friscoisd.org>
2023-03-08 22:33:07 -0600

    Added more flooring + start of new stage and water collisions

---
+++ DifficultyChoosingCamera.gd
@@ -0,0 +1,17 @@
+extends Area2D
+
+onready var body = get_parent().get_parent().get_node("YSort/Player/Camera2D")
+onready var player = get_parent().get_parent().get_node("YSort/Player")
+
+
+func _physics_process(_delta):
+	if(overlaps_body(player)): 
+		set_physics_process(false)
+		body.set_limit(0, -270)
+		body.set_limit(1, -360)
+		body.set_limit(2, 450)
+		body.set_limit(3, -180)
+
+func _ready():
+	set_physics_process(true)
+
