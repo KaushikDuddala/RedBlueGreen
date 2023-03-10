8d2bb0e9447c062989f0bf2c59eb4c0b18a39436
Kaushik Duddala <kaushik.duddala.964@k12.friscoisd.org>
2023-03-10 07:41:30 -0600

    Forgot one slide

--- BottomMiddleSign.gd
+++ BottomMiddleSign.gd
@@ -2,11 +2,21 @@ extends Area2D
 
 onready var body = get_parent().get_node("Player")
 onready var sprite = get_parent().get_node("Player/Camera2D/CanvasLayer/Balance")
+onready var slide1 = preload("res://Files/balance.png")
+onready var slide2 = preload("res://Files/description of environmental change.png")
+var currentlSlide = 0
 
 
 func _physics_process(_delta):
 	if(overlaps_body(body)): 
 		sprite.set_visible(true)
+		if(Input.is_action_just_pressed("key_tab")):
+			if(currentlSlide == 0):
+				currentlSlide = 1
+				sprite.set_texture(slide2)
+			elif(currentlSlide == 1):
+				currentlSlide = 0
+				sprite.set_texture(slide1)
 	else:
 		sprite.set_visible(false)
 
