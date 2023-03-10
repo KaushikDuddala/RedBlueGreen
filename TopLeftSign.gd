ca3619418556a33df0fd30c427d97bff89e12931
Kaushik Duddala <kaushik.duddala.964@k12.friscoisd.org>
2023-03-09 23:53:42 -0600

    Added scrollable slide + more signs/info

--- TopLeftSign.gd
+++ TopLeftSign.gd
@@ -1,12 +1,22 @@
 extends Area2D
 
 onready var body = get_parent().get_node("Player")
-onready var sprite = get_parent().get_node("Player/Camera2D/CanvasLayer/CommonName")
+onready var sprite = get_parent().get_node("Player/Camera2D/CanvasLayer/Ecosystem")
+onready var slide1 = preload("res://Files/ecosystem description.png")
+onready var slide2 = preload("res://Files/ecosystem.png")
+var currentlSlide = 0
 
 
 func _physics_process(_delta):
 	if(overlaps_body(body)): 
 		sprite.set_visible(true)
+		if(Input.is_action_just_pressed("key_tab")):
+			if(currentlSlide == 0):
+				currentlSlide = 1
+				sprite.set_texture(slide2)
+			elif(currentlSlide == 1):
+				currentlSlide = 0
+				sprite.set_texture(slide1)
 	else:
 		sprite.set_visible(false)
 
