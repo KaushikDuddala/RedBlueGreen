e92fa71ceb614023fb5f2945c871bfb7607efb48
Kaushik Duddala <kaushik.duddala.964@k12.friscoisd.org>
2023-03-10 07:32:48 -0600

    Finished all signs

---
+++ TopMiddleSign.gd
@@ -0,0 +1,26 @@
+extends Area2D
+
+onready var body = get_parent().get_node("Player")
+onready var sprite = get_parent().get_node("Player/Camera2D/CanvasLayer/FoodChainweb")
+onready var slide1 = preload("res://Files/food chain b4.png")
+onready var slide2 = preload("res://Files/past food web.png")
+var currentlSlide = 0
+
+
+func _physics_process(_delta):
+	if(overlaps_body(body)): 
+		sprite.set_visible(true)
+		if(Input.is_action_just_pressed("key_tab")):
+			if(currentlSlide == 0):
+				currentlSlide = 1
+				sprite.set_texture(slide2)
+			elif(currentlSlide == 1):
+				currentlSlide = 0
+				sprite.set_texture(slide1)
+	else:
+		sprite.set_visible(false)
+
+
+func _ready():
+	set_physics_process(true)
+
